7ca347bfcaa48dee2b7ed9219c3f36e7b0f661e7
sarikathota <sarikathota18@gmail.com>
2020-03-04 20:12:25 +0530

    my first commit

---
+++ src/pratice1.java
@@ -0,0 +1,18 @@
+
+public class pratice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="payment $100 paid";
+		System.out.println(str.charAt(8));
+		// giving index number displays the char in that particular index
+		String str="payments $100 paid";
+		System.out.println(str.indexOf("$"));
+// giving string in the argument displays the index number in the output
+		String str="payments $100 paid";
+		System.out.println(str.substring(9));
+		// giving the begin index to displays the substring
+		
+	}
+
+}
